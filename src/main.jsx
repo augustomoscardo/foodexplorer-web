f1e4dc3d4275bc008ae7bfd15828fc8241c68217
Augusto Moscardo <augustomsaluz@gmail.com>
2024-01-22 00:55:46 -0300

    add ThemeProvider, GlobalStyles, ContextProvider & Routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { ThemeProvider } from 'styled-components'
+
+import { App } from './App'
+import theme from './styles/theme'
+import GlobalStyles from './styles/global'
+import { AuthProvider } from './hooks/auth'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <App />
+        <GlobalStyles />
+      </AuthProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
