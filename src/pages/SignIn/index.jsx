6745d4381a26a26027456ab7a3e11102adf3349d
Augusto Moscardo <augusto.almeida@redeoba.com.br>
2024-01-23 17:01:41 -0300

    creating signIn page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,20 +1,41 @@
+import { useState } from 'react'
 import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
 import { Container, Logo, Form } from './styles'
 
+import logoExplorer from '../../assets/logo.svg'
+
 export function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function handleSignIn() {
+    console.log(email, password);
+  }
+
   return (
     <Container>
-      <Logo />
+      <Logo>
+        <img src={logoExplorer} alt="Logo da trilha explorer da rocketseat" />
+        <h1>food explorer</h1>
+      </Logo>
       <Form>
-        <h1>Faça login</h1>
+        <h2>Faça login</h2>
 
         <Input
           textLabel='E-mail'
           placeholder=''
-          type=''
-          onChange={ }
+          type='text'
+          onChange={e => setEmail(e.target.value)}
         />
-        <Input textLabel='Senha' />
+        <Input
+          textLabel='Senha'
+          placeholder=''
+          type=''
+          onChange={e => setPassword(e.target.value)}
+        />
+
+        <Button onClick={handleSignIn} />
       </Form>
     </Container>
   )
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -3,12 +3,48 @@ import styled from "styled-components";
 export const Container = styled.div`
   height: 100dvh;
 
+  max-width: 1368px;
+  width: 100%;
+  margin: 0 auto;
+
   display: flex;
   align-items: stretch;
+  justify-content: space-between;
+
+  padding: 140px 150px;
 `
 
 export const Logo = styled.div`
-  
+  width: 32.4rem;
+
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+
+  svg {}
+
+  h1 {
+    font-size: 4.2rem;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 700;
+  }
 `
 
-export const Form = styled.form``
+export const Form = styled.form`
+  background-color: ${({ theme }) => theme.colors.darkBlue_700};
+
+  width: 47.6rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 3.2rem;
+
+  padding: 6.4rem;
+
+  > h2 {
+    font-size: 3.2rem;
+    font-weight: 500;
+    line-height: 1.4;
+  }
+`
